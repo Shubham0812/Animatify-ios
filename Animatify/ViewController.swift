6ce06e734bd6af3d5bb47434ddcc19df5018c870
Shubham0812 <singh.shubham0812@gmail.com>
2020-06-13 18:13:44 +0530

    Initial Commit

---
+++ Animatify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animatify
+//
+//  Created by Shubham Singh on 13/06/20.
+//  Copyright © 2020 Shubham Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
