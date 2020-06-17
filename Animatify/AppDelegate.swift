9f00eca72b475b95d859d9ebe6f912025dc98535
Shubham0812 <singh.shubham0812@gmail.com>
2020-06-17 18:52:53 +0530

    Workspace Changes

--- Animatify/AppDelegate.swift
+++ Animatify/AppDelegate.swift
@@ -17,22 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         /// add logo animation for the launchScreen
         guard let window = window else { return true }
         
-        let logoView = UIView()
-        logoView.frame = window.frame
-        logoView.backgroundColor = UIColor(named: "background")
-        guard let accentColor = UIColor(named: "accentColor") else { return true }
-        let logoLayer = LogoLayer(for: logoView, scale: 2, duration: 0.95, lineWidth: 6, trackColor: accentColor, strokeColor: UIColor.white)
-        logoView.layer.insertSublayer(logoLayer, below: logoView.layer)
-        
-        window.makeKeyAndVisible()
-        window.addSubview(logoView)
-        
-        // remove the animation
-        Timer.scheduledTimer(withTimeInterval: 1.05, repeats: false) { (Timer) in
-            ViewAnimationFactory.makeEaseOutAnimation(duration: 0.25, delay: 0) {
-                logoView.layer.opacity = 0
-            }
-        }
+    
         return true
     }
 }
