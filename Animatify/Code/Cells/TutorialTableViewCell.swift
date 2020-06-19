423b3fcb1a925ed7482f15e6f968201eb69b391a
Shubham0812 <singh.shubham0812@gmail.com>
2020-06-19 15:16:51 +0530

    Tutorial tableView Cells added

--- Animatify/Code/Cells/TutorialTableViewCell.swift
+++ Animatify/Code/Cells/TutorialTableViewCell.swift
@@ -18,7 +18,6 @@ class TutorialTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
     
 }
