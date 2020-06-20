2706f0cfb647e5bcf51f2f55c62a560302b7f5b1
Shubham0812 <singh.shubham0812@gmail.com>
2020-06-20 21:11:51 +0530

    Added cell for Tutorials

--- Animatify/Code/Cells/TutorialTableViewCell.swift
+++ Animatify/Code/Cells/TutorialTableViewCell.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class TutorialTableViewCell: UITableViewCell {
-
+    
+    override class func description() -> String {
+        return "TutorialTableViewCell"
+    }
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var tutorialButton: UIButton!
+    @IBOutlet weak var tutorialLabel: UILabel!
+    @IBOutlet weak var tutorialDifficulyLabel: UILabel!
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        selectionStyle = .none
+        tutorialButton.layer.cornerRadius = tutorialButton.layer.frame.width / 2.0 - 3
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
---
+++ Animatify/Effects/ShimmerEffect.swift
@@ -0,0 +1,13 @@
+//
+//  ShimmerEffect.swift
+//  Animatify
+//
+//  Created by Shubham Singh on 20/06/20.
+//  Copyright © 2020 Shubham Singh. All rights reserved.
+//
+
+import UIKit
+
+class ShimmerEffect: CAShapeLayer {
+
+}
