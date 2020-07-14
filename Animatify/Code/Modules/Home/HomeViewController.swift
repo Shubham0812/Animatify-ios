be481a3e08201f0c9bf07d2a6e8710d9d2057978
Shubham0812 <singh.shubham0812@gmail.com>
2020-07-14 19:24:12 +0530

    Text Type & Gradient Effects

--- Animatify/Code/Modules/Home/HomeViewController.swift
+++ Animatify/Code/Modules/Home/HomeViewController.swift
@@ -35,7 +35,10 @@ class HomeViewController: UIViewController {
     
     // hardcoding the tableView data for now
     var effects: [Effect] = [
-        Effect(action: .pulse, title: "Pulse Effect", description: "Pulse effect that can be applied to different views and buttons. Color, size, and duration can be set accordingly.", instructions: ["The effect is created with CALayer, applied to the layer of the view.","For a circular pulse, turn your view into a circle and pass the radius as 2.", "You can also modify animation duration to generate another variety of pulse."], gradientColor1: UIColor(hex: "c33764"), gradientColor2: UIColor(hex: "1d2671")), Effect(action: .shimmer, title: "Shimmer Effect", description: "Shimmer Effect is an unobtrusive loading indicator that provides an engaging way to show the loading.", instructions: ["The effect is created with CAGradientLayer, applied to the layer of the view.", "You can modify the animation duration to change the pace of the shimmer."], gradientColor1: UIColor(hex: "c33764"), gradientColor2: UIColor(hex: "1d2671"))]
+        Effect(action: .textTyping, title: "Text-Typing Effects", description: "Text-Typing effects that can be applied to TextFields & TextView. The type speed can be modified as per need.", instructions: ["The effects are created by using custom functions and extension of the UIView.", "The gradients are applied to the layer of the TextView.", "Find the functions under the ViewExtensions."], gradientColor1: UIColor(hex: "43cea2"), gradientColor2: UIColor(hex: "185a9d")),
+        Effect(action: .pulse, title: "Pulse Effect", description: "Pulse effect that can be applied to different views and buttons. Color, size, and duration can be set accordingly.", instructions: ["The effect is created with CALayer, applied to the layer of the view.","For a circular pulse, turn your view into a circle and pass the radius as 2.", "You can also modify animation duration to generate another variety of pulse."], gradientColor1: UIColor(hex: "c33764"), gradientColor2: UIColor(hex: "1d2671")),
+        Effect(action: .shimmer, title: "Shimmer Effect", description: "Shimmer Effect is an unobtrusive loading indicator that provides an engaging way to show the loading.", instructions: ["The effect is created with CAGradientLayer, applied to the layer of the view.", "You can modify the animation duration to change the pace of the shimmer."], gradientColor1: UIColor(hex: "c33764"), gradientColor2: UIColor(hex: "1d2671"))
+    ]
     
     
     var tutorials: [Tutorial] = [
@@ -49,7 +52,9 @@ class HomeViewController: UIViewController {
     ]
     
     var transitions: [Transition] = [
-        Transition(title: "Circular View Transitioning", action: .circular, difficulty: "Hard", icon: "bolt.horizontal.fill")
+        Transition(title: "Circular View Transitioning", action: .circular, difficulty: "Hard", icon: "bolt.horizontal.fill"),
+        Transition(title: "Image Launch Transitioning", action: .expand, difficulty: "Medium", icon: "bolt.horizontal.fill")
+
     ]
     
     // MARK:- lifecycle methods for the viewController
@@ -136,10 +141,15 @@ extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelega
     func  collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard let effectDetailsVC = self.storyboard?.instantiateViewController(identifier: EffectDetailsViewController.description()) as? EffectDetailsViewController else { return }
         let selectedEffect = self.effects[indexPath.item]
-        if (selectedEffect.action != .none) {
+        if (selectedEffect.action != .none && selectedEffect.action != .textTyping) {
             effectDetailsVC.effect = selectedEffect
             self.navigationController?.pushViewController(effectDetailsVC, animated: true)
+        } else if (selectedEffect.action == .textTyping) {
+            guard let textTypeVC = self.storyboard?.instantiateViewController(identifier: TextTypeViewController.description()) as? TextTypeViewController else { return }
+            textTypeVC.effect = selectedEffect
+            self.navigationController?.pushViewController(textTypeVC, animated: true)
         }
+        
     }
 }
 
@@ -178,7 +188,6 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         if (tableView == tutorialsTableView) {
             DispatchQueue.main.asyncAfter(deadline: .now()) {
                 let action = self.tutorials[indexPath.row].action
-                
                 if (action == .tableViews) {
                     guard let tutorialVC = UIStoryboard(name: "Tutorial", bundle: nil).instantiateViewController(withIdentifier: TableAnimationViewController.description()) as? TableAnimationViewController else { return }
                     self.present(tutorialVC, animated: true)
@@ -208,8 +217,13 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
             }
         } else {
             DispatchQueue.main.asyncAfter(deadline: .now()) {
-                if (self.transitions[indexPath.row].action == .circular) {
+                let action = self.transitions[indexPath.row].action
+                if (action == .circular) {
                     guard let transitionVC = UIStoryboard(name: "Transition", bundle: nil).instantiateViewController(withIdentifier: CircleViewController.description()) as? CircleViewController else { return }
+                    transitionVC.modalPresentationStyle = .fullScreen
+                    self.present(transitionVC, animated: true)
+                } else if (action == .expand) {
+                    guard let transitionVC = UIStoryboard(name: "Transition", bundle: nil).instantiateViewController(withIdentifier: CharactersViewController.description()) as? CharactersViewController else { return }
                     transitionVC.modalPresentationStyle = .fullScreen
                     self.present(transitionVC, animated: true)
                 }
