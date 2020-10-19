c7243de491c5c2328ac8a0ab39a356b9ba5531d2
Sai Raghu Varma <srvarma7@gmail.com>
2020-10-19 14:52:14 +1100

    Adding View extension for auto layout

--- Animatify/Extensions/ViewExtensions.swift
+++ Animatify/Extensions/ViewExtensions.swift
@@ -14,6 +14,48 @@ extension UIView {
         self.layer.cornerRadius = cornerRadius
         self.clipsToBounds = true
     }
+    
+    func addAnchor(top: NSLayoutYAxisAnchor?, paddingTop: CGFloat,
+                left: NSLayoutXAxisAnchor?, paddingLeft: CGFloat,
+                bottom: NSLayoutYAxisAnchor?, paddingBottom: CGFloat,
+                right: NSLayoutXAxisAnchor?, paddingRight: CGFloat,
+                width: CGFloat, height: CGFloat,
+                enableInsets: Bool) {
+        
+        var topInset = CGFloat(0)
+        var bottomInset = CGFloat(0)
+        
+        if #available(iOS 11, *), enableInsets {
+            let insets = self.safeAreaInsets
+            topInset = insets.top
+            bottomInset = insets.bottom
+//
+//            print("Top: \(topInset)")
+//            print("bottom: \(bottomInset)")
+        }
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop+topInset).isActive = true
+        }
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom-bottomInset).isActive = true
+        }
+        if height != 0 {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+        if width != 0 {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+    }
 }
 
 extension UILabel {
